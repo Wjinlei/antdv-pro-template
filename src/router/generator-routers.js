c83fbe6cec87e438650418a49bc6661e837f1310
Jerry Wang <freebufer.wang@gmail.com>
2022-05-18 20:15:42 +0800

    Modify import view path /views to /views/example

--- src/router/generator-routers.js
+++ src/router/generator-routers.js
@@ -10,9 +10,9 @@ const constantRouterComponents = {
   BlankLayout: BlankLayout,
   RouteView: RouteView,
   PageView: PageView,
-  '403': () => import(/* webpackChunkName: "error" */ '@/views/exception/403'),
-  '404': () => import(/* webpackChunkName: "error" */ '@/views/exception/404'),
-  '500': () => import(/* webpackChunkName: "error" */ '@/views/exception/500'),
+  403: () => import(/* webpackChunkName: "error" */ '@/views/example/exception/403'),
+  404: () => import(/* webpackChunkName: "error" */ '@/views/example/exception/404'),
+  500: () => import(/* webpackChunkName: "error" */ '@/views/example/exception/500'),
 
   // 你需要动态引入的页面组件
   Workplace: () => import('@/views/example/dashboard/Workplace'),
@@ -39,9 +39,9 @@ const constantRouterComponents = {
   ResultFail: () => import(/* webpackChunkName: "result" */ '@/views/example/result/Error'),
 
   // exception
-  Exception403: () => import(/* webpackChunkName: "fail" */ '@/views/exception/403'),
-  Exception404: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404'),
-  Exception500: () => import(/* webpackChunkName: "fail" */ '@/views/exception/500'),
+  Exception403: () => import(/* webpackChunkName: "fail" */ '@/views/example/exception/403'),
+  Exception404: () => import(/* webpackChunkName: "fail" */ '@/views/example/exception/404'),
+  Exception500: () => import(/* webpackChunkName: "fail" */ '@/views/example/exception/500'),
 
   // account
   AccountCenter: () => import('@/views/example/account/center'),
